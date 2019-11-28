75ce5a262b00a4dc981d36b1cfb95d786d694e0c
Lance Smith <lance.matthew.smith@gmail.com>
2019-11-27 16:45:28 -0800

    Start with templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Lance'}
+    posts = [
+        {
+            'author': {'username': 'Lance'},
+            'body': 'Tight, tight, tight.'
+        },
+        {
+            'author': {'username': 'Slippy'},
+            'body': 'Bwop, bwop, BWAHHP'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
