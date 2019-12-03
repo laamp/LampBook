4d5d3665a68b6428e61232e3c5b78bb841b68698
Lance Smith <lance.matthew.smith@gmail.com>
2019-12-03 00:14:57 -0800

    Create followers table

--- app/models.py
+++ app/models.py
@@ -11,6 +11,14 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+# table for user's following other users
+followers = db.Table(
+    "followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id")),
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -20,6 +28,15 @@ class User(UserMixin, db.Model):
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    followed = db.relationship(
+        "User",
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -34,6 +51,24 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
             digest, size
+        )
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        return (
+            Post.query.join(followers, (followers.c.followed_id == Post.user_id))
+            .filter(followers.c.follower_id == self.id)
+            .order_by(Post.timestamp.desc())
         )
 
 
