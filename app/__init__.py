9a7e45c762d563dcca4be555c9266a8021cd149c
Lance Smith <lance.matthew.smith@gmail.com>
2019-12-02 17:01:14 -0800

    Add validation for duplicate usernames after account is created

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+import os
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -34,5 +35,23 @@ if not app.debug:
 
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    file_handler = RotatingFileHandler(
+        "logs/lampbook.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("LampBook startup")
+
 
 from app import routes, models, errors
